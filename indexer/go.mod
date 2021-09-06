815b606ea98d15fb99a7ed3f7f8bd7bd1911591a
Howard Wang <joy_luck_2000@hotmail.com>
2021-09-05 22:55:54 -0700

    Add deployments for ElasticSearch cluster (#5)
    * add ElasticSearch cluster creation
    
    * add env variable, remove python code
    
    * fix: fix auth for ES client
    
    * update main.go

--- indexer/go.mod
+++ indexer/go.mod
@@ -1,9 +1,8 @@
 module github.com/howardwang15/elasticsearch-nba/indexer
 
 require (
-	github.com/elastic/go-elasticsearch v0.0.0 // indirect
 	github.com/elastic/go-elasticsearch/v8 v8.0.0-20210729094743-e8d7ea3ceeed
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 )
 
 go 1.15
--- indexer/main.go
+++ indexer/main.go
@@ -3,33 +3,42 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/joho/godotenv"
 )
 
 func IndexDocuments(documents Documents, es *elasticsearch.Client) {
-	const INDEX_NAME string = "players"
-	_, err := es.Indices.Delete([]string{INDEX_NAME})
+	// Get date for index creation
+	now := time.Now().Format("20060102")
+	indexName := "players-" + now
+
+	// Delete index if it already exists
+	_, err := es.Indices.Delete([]string{indexName})
 	if err != nil {
 		panic(err)
 	}
 
+	// Read mapping and create index
 	mappingRaw, _ := ioutil.ReadFile("./mapping.json")
 	var data interface{}
 	_ = json.Unmarshal(mappingRaw, &data)
 	mapping, _ := json.Marshal(data)
 	_, err = es.Indices.Create(
-		INDEX_NAME,
+		indexName,
 		es.Indices.Create.WithBody(strings.NewReader(string(mapping))))
+	if err != nil {
+		panic(err)
+	}
 
+	// Index all documents
 	for i, doc := range documents.Documents {
 		encoded, _ := json.Marshal(doc)
-		es.Index(INDEX_NAME,
+		es.Index(indexName,
 			strings.NewReader(string(encoded)),
 			es.Index.WithDocumentID(strconv.Itoa(i+1)),
 		)
@@ -37,10 +46,9 @@ func IndexDocuments(documents Documents, es *elasticsearch.Client) {
 }
 
 func main() {
-	es := CreateESClient()
 	godotenv.Load()
+	es := CreateESClient()
 	baseDataPath := os.Getenv("DATA_PATH")
-	log.Println(es.Info())
 	documents := ReadDocuments(baseDataPath)
 	IndexDocuments(documents, es)
 }
