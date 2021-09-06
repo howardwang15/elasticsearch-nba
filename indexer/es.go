d07193c238059fd7dafd31dc7fc7d03c70ada3eb
Howard Wang <joy_luck_2000@hotmail.com>
2021-09-05 22:08:48 -0700

    fix: fix auth for ES client

--- indexer/es.go
+++ indexer/es.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/estransport"
 )
 
 func CreateESClient() *elasticsearch.Client {
-	es, err := elasticsearch.NewDefaultClient()
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	config := elasticsearch.Config{
+		Addresses: []string{"https://localhost:9200"},
+		Username:  os.Getenv("ELASTIC_USERNAME"),
+		Password:  os.Getenv("ELASTIC_PASSWORD"),
+		Logger:    &estransport.ColorLogger{Output: os.Stdout},
+		Transport: transport,
+	}
+	es, err := elasticsearch.NewClient(config)
 	if err != nil {
 		panic(err)
 	}
-	log.Println("Starting Elasticsearch client...")
-	log.Println(elasticsearch.Version)
+	log.Println("Starting Elasticsearch client...version", elasticsearch.Version)
+	log.Println(es.Cluster.Health())
 	return es
 }
